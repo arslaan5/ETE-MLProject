c7925dcc539c2bbd76405c5d41b04a6d662804f7
Arslaan Siddiqui <arslaansiddiqui23@gmail.com>
2024-09-25 18:17:46 +0530

    Coded data_transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -49,3 +50,6 @@ class DataIngestion:
 if  __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
