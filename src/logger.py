1725ca59037a7e1620ccb1cbb14ac6526c091fd0
Arslaan Siddiqui <arslaansiddiqui23@gmail.com>
2024-09-27 16:34:49 +0530

    Final app.py created

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
-log_path =  os.path.join(os.getcwd(), "logs", LOG_FILE)
+log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(log_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
